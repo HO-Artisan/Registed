32682fbffcff896c6e97b79f1bfadc21cc002c18
KrLite <68179735+KrLite@users.noreply.github.com>
2023-10-01 09:31:00 +0800

    update ids

---
+++ src/main/java/ho/artisan/registed/Registed.java
@@ -0,0 +1,20 @@
+package ho.artisan.registed;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Registed implements ModInitializer {
+	public static final String NAME = "Registed", ID = "registed";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+	@Override
+	public void onInitialize() {
+	}
+
+	public static Identifier id(String... paths) {
+		return new Identifier(ID, String.join("/", paths));
+	}
+}
